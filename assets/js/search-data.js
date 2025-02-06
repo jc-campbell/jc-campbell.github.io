427da8851ff7e1bf131657308962325fc7ff2000
jc-campbell <155665256+jc-campbell@users.noreply.github.com>
2025-02-06 23:30:27 +0000

    Deploying to gh-pages from @ jc-campbell/jc-campbell.github.io@3160fe567693f5572398e0e50fa137e2ee8d507e 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -466,6 +466,11 @@ ninja.data = [{
           description: "another project with an image 🎉",
           section: "Projects",handler: () => {
               window.location.href = "/projects/9_project/";
+            },},{id: "projects-math-financial-economics",
+          title: 'Math Financial Economics',
+          description: "with background image",
+          section: "Projects",handler: () => {
+              window.location.href = "/projects/financial_econ/";
             },},{
         id: 'social-email',
         title: 'email',
